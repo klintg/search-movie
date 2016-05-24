543cf54d60b9ed3f1ca3bc64bc4858391ebfbfb8
klintg <theklint1@gmail.com>
2016-05-24 18:51:15 +0300

    Added a Procfile.

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 var express = require('express')
 var app = express()
+
+app.use(express.static(__dirname + ('/dist')))
+
+var port = process.env.PORT || 3000
+app.listen(port)
+console.log("App running on port", port)
